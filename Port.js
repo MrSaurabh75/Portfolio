9823bd58d6b5b213804f5753e02da542ab7a41c9
Saurabh Chorge <saurabhdc7575@gmail.com>
2023-10-17 12:20:30 +0530

    Add files via upload

---
+++ Port.js
@@ -0,0 +1,14 @@
+
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname) {
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-links");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-links");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
